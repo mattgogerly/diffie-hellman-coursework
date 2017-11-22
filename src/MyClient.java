9d61aa8aebb2e34c8a41ce1fcfd0a23852c71698
Matt <mg11g16@soton.ac.uk>
2017-11-22 18:54:45 +0000

    Changed to be a mainline server handling ClientImplementation

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,142 +1,46 @@
-import java.math.BigInteger;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
+
+// https://www.e-reading.club/chapter.php/71303/10/Milne_-_The_house_at_Pooh_Corner.html
 
 public class MyClient {
 	
-	private String serverAddr;
-	
-	private KeyInterface ki;
-	private BigInteger b, p, g;
-	private BigInteger x;
-	private BigInteger secretKey;
-	
-	private String cipherText;
+	private static Registry reg;
 	
 	public static void main(String[] args) {
-		if (args.length == 2) {
-			MyClient client = new MyClient(args[0], args[1]);
+		if (args.length != 2) {
+			return;
 		} else {
-			System.exit(-1);
-		}
-	}
-	
-	public MyClient(String serverAddr, String uid) {
-		this.serverAddr = serverAddr;
-		
-		establishSecureConnection();
-		
-		try {
-			cipherText = ki.getCiphertext(uid);
-			System.out.println(cipherText);
-			decryptCiphertext();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		Scanner scan = new Scanner(System.in);
-		scan.nextLine();
-		scan.close();
-	}
-	
-	private boolean establishSecureConnection() {
-		try {
-			System.out.println("Attempting to establish secure connection with the server...");
-			
-			Registry reg = LocateRegistry.getRegistry(serverAddr);
-			ki = (KeyInterface) reg.lookup("Key");
-			
-			p = ki.getP();
-			g = ki.getG();
-			x = ki.calculateX();
-			
-			Random rand = new Random();
-			b = BigInteger.valueOf(rand.nextInt(20) + 1);
-			
-			BigInteger y = g.modPow(b, p);
-			ki.calculateKey(y);
-			
-			secretKey = x.modPow(b, p);
-			
-			if (!ki.checkSameSecret(secretKey)) {
-				throw new Exception("The private keys do not match, secure connection not established!");
-			}
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-			return false;
-		}
-		
-		System.out.println("Secure connection established!");
-		return true;
-	}
-	
-	private void decryptCiphertext() {
-		int shuffleDistance = secretKey.mod(BigInteger.valueOf(8)).intValue();
-		
-		List<char[]> chunks = new ArrayList<char[]>();
-		
-		int i = 0;
-		while (i < cipherText.length()) {
-			char[] chunk = cipherText.substring(i, Math.min(i + 8, cipherText.length())).toCharArray();
-			chunks.add(chunk);
-			i += 8;
-		}
-		
-		for (int j = 0; j < chunks.size(); j++) {	
-			char[] current = chunks.get(j);
-			
-			deSubstitute(current);
-			deTranspose(current, shuffleDistance);
-			deTranspose(current, shuffleDistance);
-		}
-		
-		for (int j = 0; j < chunks.size(); j++) {
-			char[] current = chunks.get(j);
-			
-			for (int k = 0; k < 8; k++) {
-				System.out.print(current[k]);
-			}
-			
-			System.out.println();
-		}
-	}
-	
-	private void deTranspose(char[] characters, int distance) {
-		char[] temp = new char[characters.length];
-		
-		System.arraycopy(characters, distance, temp, 0, characters.length - distance);
-	    System.arraycopy(characters, 0, temp, characters.length - distance, distance);
-	    
-	    System.arraycopy(temp, 0, characters, 0, characters.length);
-	}
-	
-	private void deSubstitute(char[] characters) {
-		int distance = secretKey.mod(BigInteger.valueOf(26)).intValue();
-		
-		for (int i = 0; i < characters.length; i++) {
-			char current = characters[i];
-			
-			if (current >= 'a' && current <= 'z') {
-				current = (char) (current - distance);
-				
-				if (current <'a') {
-					current = (char) (current + 'z' - 'a' + 1);
+			try {
+				System.setProperty( "java.security.policy", "SecurityPolicy" );
+				if (System.getSecurityManager() == null) {
+				    System.setSecurityManager(new SecurityManager());
 				}
 				
-				characters[i] = current;
-			} else if (current >= 'A' && current <= 'Z') {
-				current = (char) (current - distance);
+				String hostname = args[0];
+				String uid = args[1];
 				
-				if (current <'A') {
-					current = (char) (current + 'Z' - 'A' + 1);
-				}
+				ClientImplementation client = new ClientImplementation();
+				ClientInterface clientStub = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);
+				reg = LocateRegistry.getRegistry(hostname);
+			    KeyInterface ki = (KeyInterface) reg.lookup("Key");
 				
-				characters[i] = current;
+			    ki.calculateKey(clientStub);
+			    if (ki.checkSameSecret(clientStub, clientStub.getSecretKey())) {
+			    	System.out.println("Secure connection to server established!");
+			    }
+			    
+			    ki.getCiphertext(clientStub, uid);
+			    
+			    Scanner scan = new Scanner(System.in);
+			    scan.nextLine();
+			    scan.close();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
+	
 }

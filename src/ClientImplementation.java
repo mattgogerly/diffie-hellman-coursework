9bef1f8b48da7fc4de9a728fd9d73bc6f83a858c
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-11-23 18:31:27 +0000

    Began commenting and removed unnecessary volatile boolean

--- src/ClientImplementation.java
+++ src/ClientImplementation.java
@@ -5,31 +5,50 @@ import java.util.Random;
 
 // https://www.e-reading.club/chapter.php/71303/10/Milne_-_The_house_at_Pooh_Corner.html
 
+/**
+ * An implementation of the ClientInterface remote interface.
+ * 
+ * @see ClientInterface
+ * 
+ * @author Matt Gogerly
+ * @version 1.0
+ */
+
 public class ClientImplementation implements ClientInterface {
 	
+	// BigIntegers to store values calculated during the calculating of the secret key
 	private BigInteger b, p, g;
 	private BigInteger x;
 	private BigInteger y;
+	
+	// int to store the calculated secret key
 	private int secretKey;
 	
-	public volatile boolean calculating = false;
-	
+	// The ciphertext retreived from the server
 	private String cipherText;
 	
+	/**
+	 * Constructor for ClientImplementation. We don't need to store any values so we leave it empty.
+	 */
 	public ClientImplementation() {
-		//
+
 	}
 	
+	/**
+	 * Method to calculate the private key. 
+	 * 
+	 * This method is called by the server only once p, g and x have been set.
+	 */
 	public boolean calculateKey() {
-		try {			
-			calculating = true;
-		
+		try {		
+			// Generate a random number b
 			Random rand = new Random();
 			b = BigInteger.valueOf(rand.nextInt(20) + 1);
 			
+			// Calculate y (g^b % p) and set calculating back to false (so server can get)
 			y = g.modPow(b, p);
-			calculating = false;
 			
+			// Calculate the secret key on the client side (x^b % p)
 			BigInteger tempKey = x.modPow(b, p);
 			secretKey = tempKey.intValue();
 		} catch (Exception e) {
@@ -40,27 +59,35 @@ public class ClientImplementation implements ClientInterface {
 		return true;
 	}
 	
+	/**
+	 * Method to set X value.
+	 * 
+	 * @param x: the value for x calculated by the remote server.
+	 */
 	public void setX(BigInteger x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Method to set p value.
+	 * 
+	 * @param p: the value for p provided by the remote server.
+	 */
 	public void setP(BigInteger p) {
 		this.p = p;
 	}
 	
+	/**
+	 * Method to set g value.
+	 * 
+	 * @param p: the value for g provided by the remote server.
+	 */
 	public void setG(BigInteger g) {
 		this.g = g;
 	}
 	
-	public BigInteger getY() {
-		while (calculating) {
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-		
+	
+	public BigInteger getY() {		
 		return y;
 	}
 	
@@ -96,17 +123,11 @@ public class ClientImplementation implements ClientInterface {
 			deTranspose(current, shuffleDistance);
 		}
 		
-		int l = 0;
 		for (int j = 0; j < chunks.size(); j++) {
 			char[] current = chunks.get(j);
 			
 			for (int k = 0; k < 8; k++) {
-				System.out.print(current[k] + " ");
-			}
-			
-			l++;
-			if (l % 3 == 0) {
-				System.out.println();
+				System.out.print(current[k]);
 			}
 		}
 	}

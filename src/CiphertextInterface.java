a1e33b9a9a9c4bbbbc8e628c33758776f51cfb53
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-11-23 18:31:00 +0000

    Commenting

--- src/CiphertextInterface.java
+++ src/CiphertextInterface.java
@@ -1,6 +1,15 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * 
+ * The provided interface for the stub running on the specified remote
+ * server. Provides the means to get a ciphertext, encrypted using
+ * a provided user id and secret key.
+ * 
+ * @author Tim Norman, University of Southampton
+ */
+
 public interface CiphertextInterface extends Remote {
 	
 	public String get(String uid , int key) throws RemoteException;

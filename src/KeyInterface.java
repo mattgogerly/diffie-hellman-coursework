33c2790d2f8ba46f83107a4a0aafd40c3781467a
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-11-23 18:32:12 +0000

    Throwing generic exception

--- src/KeyInterface.java
+++ src/KeyInterface.java
@@ -8,7 +8,7 @@ public interface KeyInterface extends Remote {
 	public BigInteger getP() throws RemoteException;
 	public BigInteger getG() throws RemoteException;
 	
-	public void calculateKey(ClientInterface client) throws RemoteException;
+	public void calculateKey(ClientInterface client) throws RemoteException, Exception;
 	public boolean checkSameSecret(ClientInterface client, int key) throws RemoteException;
 	
 	public void getCiphertext(ClientInterface client, String uid) throws RemoteException;
